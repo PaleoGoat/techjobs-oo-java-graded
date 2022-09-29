22589d80e067193e79e2a2f99950698821b4793c
Max Jewett <maximo.jewett@gmail.com>
2022-09-28 21:41:10 -0500

    Task 4 complete

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,50 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+
+    @Test
+    public void testSettingJobID(){
+        Job job1 = new Job();
+        Job job2 = new Job();
+        assertNotEquals(job1.getId(), job2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        // test  int, String, Employer, Location, PositionType, and CoreCompetency
+        // test for both the CLASS *and* VALUE  ---  should have 5 assert statements of each type
+        Job job2 = new Job();
+        assertEquals(job1.getId(), job2.getId()-1); //id assertEquals
+        assertTrue(((Integer) job1.getId()) instanceof Integer); //id assertTrue id is an int
+        assertEquals(job1.getName(), "Product tester"); //name assertEquals the argument "Product tester"
+        assertTrue(job1.getName() instanceof String); //name assertTrue name is a String
+        assertEquals(job1.getEmployer().getValue(),"ACME");//employer assertEquals the argument
+        assertTrue((job1.getEmployer() instanceof Employer)); //employer assertTrue is Employer class
+        assertEquals(job1.getLocation().getValue(), "Desert");//location assertEquals
+        assertTrue(job1.getLocation() instanceof Location); //location assertTrue
+        assertEquals(job1.getPositionType().getValue(), "Quality control"); //positionType assertEquals
+        assertTrue(job1.getPositionType() instanceof PositionType); //positionType assertTrue
+        assertEquals(job1.getCoreCompetency().getValue(), "Persistence"); //coreCompetency assertEquals
+        assertTrue(job1.getCoreCompetency() instanceof CoreCompetency); //coreCompetency assertTrue
+    }
+
+    @Test
+    public void testJobsForEquality(){
+        Job job1 = new Job();
+        Job job2 = new Job();
+        assertFalse(job1.equals(job2));
+    }
 
 }
--- src/test/java/org/launchcode/techjobs/oo/TestTaskFour.java
+++ src/test/java/org/launchcode/techjobs/oo/TestTaskFour.java
@@ -11,6 +11,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by LaunchCode
