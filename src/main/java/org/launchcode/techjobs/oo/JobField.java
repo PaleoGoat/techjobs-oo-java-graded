07914e91a8bc761b139e5e3ae7aac0d458926e84
Max Jewett <maximo.jewett@gmail.com>
2022-09-29 13:26:50 -0500

    Minor fixes may please autograder

--- src/main/java/org/launchcode/techjobs/oo/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -25,17 +25,30 @@ public abstract class JobField {
     }
 
     @Override
-    public boolean equals(Object o) {  // Two objects are equal if they have the same id.
+    public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Employer)) return false;
-        Employer employer = (Employer) o; //creates test employer for comparison on next line
-        return getId() == employer.getId();
+        if (!(o instanceof JobField)) return false;
+        JobField jobField = (JobField) o;
+        return getId() == jobField.getId();
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(getId());
     }
+
+    //    @Override
+//    public boolean equals(Object o) {  // Two objects are equal if they have the same id.
+//        if (this == o) return true;
+//        if (!(o instanceof Employer)) return false;
+//        Employer employer = (Employer) o; //creates test employer for comparison on next line
+//        return getId() == employer.getId();
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(getId());
+//    }
 
     // Getters and Setters:
 
